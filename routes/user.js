9cfa8869f876e3a6b0e04a3201ed99f49704c24b
Nikita Zavalei <nikitos290@gmail.com>
2013-06-11 19:14:58 +0300

    Добавил со славиком обработчик в app.js и в файле user.js и сайт перестал работать

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
-
 /*
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+exports.list = function (req, res) {
+    res.send("respond with a resource");
 };
+export.
+create = function (req, res) {
+    var userAttributes = req.param('client', null);
+    console.log(userAttributes);
+}
