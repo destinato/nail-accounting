68321fe5fa9834c9143951e9dfff2b5b65e81dae
Nikita Zavalei <nikitos290@gmail.com>
2013-06-12 17:11:37 +0300

    сделал сохранение юзера в базу

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,23 @@
 /*
  * GET users listing.
  */
+var mongoose = require('mongoose');
+var UserSchema = mongoose.Schema({
+    name: String,
+    last_name: String,
+    phone: String,
+    sum: String
+})
+
+var User = mongoose.model('User', UserSchema)
 
 exports.list = function (req, res) {
     res.send("respond with a resource");
 };
 exports.create = function (req, res) {
     var userAttributes = req.param('client', null);
-    var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/test');
-    console.log(userAttributes);
+    mongoose.connect('mongodb://admin:adminnikita@ds029658.mongolab.com:29658/nail_accounting');
+    var user = new User(userAttributes);
+    user.save();
+    res.redirect('/');
 }
