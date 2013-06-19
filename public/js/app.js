7b1e4eba9fa03c47d30a024c603fd69868a2dc31
Nikita Zavalei <nikitos290@gmail.com>
2013-06-19 14:55:05 +0300

    неполучается подключить js/app.js

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Admin
+ * Date: 19.06.13
+ * Time: 14:43
+ * To change this template use File | Settings | File Templates.
+ */
+var a = 4;
+var b = 7;
+var c = a+b;
+console.log(c)
+
